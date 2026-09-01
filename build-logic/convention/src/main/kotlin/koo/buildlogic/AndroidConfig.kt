6b29dae47e3fdb98cab70fb5f92053be6aedb9ee
ArthurKun <16458204+ArthurKun21@users.noreply.github.com>
2026-09-01 21:41:37 +0800

    feat: update compile sdk to 37

--- build-logic/convention/src/main/kotlin/koo/buildlogic/AndroidConfig.kt
+++ build-logic/convention/src/main/kotlin/koo/buildlogic/AndroidConfig.kt
@@ -4,8 +4,8 @@ import org.gradle.api.JavaVersion as GradleJavaVersion
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget as KotlinJvmTarget
 
 object AndroidConfig {
-    const val COMPILE_SDK = 36
-    const val TARGET_SDK = 36
+    const val COMPILE_SDK = 37
+    const val TARGET_SDK = 37
     const val MIN_SDK = 26
 
     // https://youtrack.jetbrains.com/issue/KT-66995/JvmTarget-and-JavaVersion-compatibility-for-easier-JVM-version-setup
